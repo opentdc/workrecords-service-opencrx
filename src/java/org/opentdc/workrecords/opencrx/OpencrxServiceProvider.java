3a64af5a2177a81d3a52133718af7caec63684cf
Werner Froidevaux <wfro@users.sourceforge.net>
2015-04-03 17:57:50 +0200

    Clean-up phase 7

--- src/java/org/opentdc/workrecords/opencrx/OpencrxServiceProvider.java
+++ src/java/org/opentdc/workrecords/opencrx/OpencrxServiceProvider.java
@@ -26,23 +26,19 @@ package org.opentdc.workrecords.opencrx;
 import java.util.ArrayList;
 import java.util.logging.Logger;
 
-import javax.jdo.PersistenceManager;
-import javax.jdo.PersistenceManagerFactory;
 import javax.naming.NamingException;
 import javax.servlet.ServletContext;
 
 import org.openmdx.base.exception.ServiceException;
-import org.openmdx.base.naming.Path;
+import org.opentdc.opencrx.AbstractOpencrxServiceProvider;
 import org.opentdc.service.exception.DuplicateException;
 import org.opentdc.service.exception.NotFoundException;
 import org.opentdc.service.exception.NotImplementedException;
 import org.opentdc.workrecords.ServiceProvider;
 import org.opentdc.workrecords.WorkRecordModel;
 
-public class OpencrxServiceProvider implements ServiceProvider {
+public class OpencrxServiceProvider extends AbstractOpencrxServiceProvider implements ServiceProvider {
 	
-	public static final String XRI_ACTIVITY_SEGMENT = "xri://@openmdx*org.opencrx.kernel.activity1";
-	public static final String XRI_ACCOUNT_SEGMENT = "xri://@openmdx*org.opencrx.kernel.account1";
 	public static final short ACTIVITY_GROUP_TYPE_PROJECT = 40;
 	public static final short ACCOUNT_ROLE_CUSTOMER = 100;
 	public static final short ACTIVITY_CLASS_INCIDENT = 2;
@@ -50,47 +46,14 @@ public class OpencrxServiceProvider implements ServiceProvider {
 	public static final short ICAL_CLASS_NA = 0;
 	public static final short ICAL_TYPE_VEVENT = 1;
 
-	private static PersistenceManagerFactory pmf = null;
-	private static String providerName = null;
-	private static String segmentName = null;
-	private static org.opencrx.kernel.activity1.jmi1.Segment activitySegment = null;
-	private static String url = null;
-	private static String userName = null;
-	private static String password = null;
-	private static String mimeType = null;
-
 	// instance variables
 	protected static final Logger logger = Logger.getLogger(OpencrxServiceProvider.class.getName());
 	
 	public OpencrxServiceProvider(
 		ServletContext context,
 		String prefix
-	) {
-		logger.info("> OpencrxImpl()");
-
-		if (url == null) {
-			url = context.getInitParameter("backend.url");
-		}
-		if (userName == null) {
-			userName = context.getInitParameter("backend.userName");
-		}
-		if (password == null) {
-			password = context.getInitParameter("backend.password");
-		}
-		if (mimeType == null) {
-			mimeType = context.getInitParameter("backend.mimeType");
-		}
-		if (providerName == null) {
-			providerName = context.getInitParameter("backend.providerName");
-		}
-		if (segmentName == null) {
-			segmentName = context.getInitParameter("backend.segmentName");
-		}
-		if (activitySegment == null) {
-			activitySegment = getActivitySegment(getPersistenceManager());
-		}
-
-		logger.info("OpencrxImpl() initialized");
+	) throws ServiceException, NamingException {
+		super(context, prefix);
 	}
 
 	/******************************** workrecord *****************************************/
@@ -178,41 +141,4 @@ public class OpencrxServiceProvider implements ServiceProvider {
 		// return _count;
 	}
 
-
-	/******************************** utility methods *****************************************/
-	/**
-	 * Get persistence manager for configured user.
-	 *
-	 * @return the PersistenceManager
-	 * @throws ServiceException
-	 * @throws NamingException
-	 */
-	public PersistenceManager getPersistenceManager() {
-
-		if (pmf == null) {
-			try {
-				pmf = org.opencrx.kernel.utils.Utils
-						.getPersistenceManagerFactoryProxy(url, userName,
-								password, mimeType);
-			} catch (NamingException e) {
-				e.printStackTrace();
-			} catch (ServiceException e) {
-				e.printStackTrace();
-			}
-		}
-		return pmf.getPersistenceManager(userName, null);
-	}
-
-	/**
-	 * Get activity segment.
-	 * 
-	 * @param pm
-	 * @return
-	 */
-	public static org.opencrx.kernel.activity1.jmi1.Segment getActivitySegment(
-			PersistenceManager pm) {
-		return (org.opencrx.kernel.activity1.jmi1.Segment) pm
-				.getObjectById(new Path(XRI_ACTIVITY_SEGMENT).getDescendant(
-						"provider", providerName, "segment", segmentName));
-	}
 }
